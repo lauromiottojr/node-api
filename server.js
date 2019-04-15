8febe4e08e43eb4205197130539df8ee7700f05d
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-15 13:36:47 -0300

    Connection with MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Inciando o APP
 const app = express();
 
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
+// Primera rota
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
