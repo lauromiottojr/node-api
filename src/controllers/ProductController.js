7e2fd29203ff449a826c511b3b25582d619f18f7
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-15 17:10:00 -0300

    CRUD completed

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -7,9 +7,24 @@ module.exports = {
         return res.json(products);
     },
 
-    async store(req, res){
+    async show(req, res) {
+        const product = await Product.findById(req.params.id);
+        return res.json(product);
+    },
+
+    async store(req, res) {
         // New
         const product = await Product.create(req.body);
         return res.json(product);
-    }
+    },
+
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        return res.json(product);
+    },
+
+    async destroy(req, res) {
+        await Product.findByIdAndRemove(req.params.id);
+        return res.send('Item deletado com sucesso!');
+    },
 };
