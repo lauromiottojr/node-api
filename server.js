0f183de30e5764d2a1ac84117cfb09eedc89e67c
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-15 17:39:52 -0300

    Making the public project

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 // Inciando o APP
 const app = express();
 app.use(express.json());
+app.use(cors()); // libera acesso para todos os domínios, não somente local
 
 // Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
